804d5fd539237ecfd51dc68f9b17d9d0f74a5ef3
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-21 23:31:43 +0500

    .

--- decreasing-triangle-program/main.py
+++ decreasing-triangle-program/main.py
@@ -1,7 +1,17 @@
-# Basic
+# Basic For loops
 n = 5 
 for i in range(n):
     for j in range(i,n):
         print("*",end=" ")
     print()
  
+# In while loops
+
+i = 5 
+while i >= 1:
+    j = 1 
+    while j<= i:
+      print("*",end = " ")
+      j = j + 1
+    print()  
+    i = i - 1
