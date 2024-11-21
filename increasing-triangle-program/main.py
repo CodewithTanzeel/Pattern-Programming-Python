169ecf13e0e931c84c13acc9891918a835691bc0
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-21 22:59:40 +0500

    .

---
+++ increasing-triangle-program/main.py
@@ -0,0 +1,15 @@
+# Basic implementation
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=' ')
+    print()
+
+# improvised
+row = int(input("number of rows :"))
+sym = input("Input whatever you want to print : ")
+for i in range(row):
+    for j in range(i + 1):
+        print(sym,end=" ")
+    print()
