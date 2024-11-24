bebcecd171a434af5bf8d2751c79564912a3331d
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-24 09:27:25 +0500

    incresing

--- increasing-triangle-program/while.py
+++ increasing-triangle-program/while.py
@@ -1,13 +1,10 @@
 
 # In while Loop Basic
-
-
-i = 5 
-while i >= 1:
-    j = 1 
-    while j<= i:
-      print("*",end = " ")
-      j = j - 1
-    print()  
-    i = i + 1
-
+i = 1
+while i <= 5:
+    j = 0
+    while j < i:
+        print("*",end="")
+        j += 1
+    i += 1
+    print() 
