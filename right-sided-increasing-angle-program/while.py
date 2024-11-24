59b32f9a360d2571c38dbe43aed83e52ba70c567
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-24 10:25:56 +0500

    .

--- right-sided-increasing-angle-program/while.py
+++ right-sided-increasing-angle-program/while.py
@@ -1,9 +1,23 @@
 # In while Loop Basic
+
 i = 1
 while i <= 5:
     j = 0
-    while j < i:
-        print("*",end=" ")
+    while j < 5 - i:
+        print(" ",end=" ")
         j += 1
-    i += 1
+    p = 0
+    while p < i:
+        print("*",end=" ")
+        p  += 1
     print() 
+    i += 1
+
+
+#Ouput 
+#         * 
+#       * * 
+#     * * *
+#   * * * *
+# * * * * *
+
