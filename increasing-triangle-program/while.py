2ce10ff668faf322a5a88a765bcc862c555e05bb
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-24 10:39:18 +0500

    .

--- increasing-triangle-program/while.py
+++ increasing-triangle-program/while.py
@@ -4,7 +4,13 @@ i = 1
 while i <= 5:
     j = 0
     while j < i:
-        print("*",end="")
+        print("*",end=" ")
         j += 1
     i += 1
     print() 
+# Output
+# * 
+# * * 
+# * * *
+# * * * *
+# * * * * *
