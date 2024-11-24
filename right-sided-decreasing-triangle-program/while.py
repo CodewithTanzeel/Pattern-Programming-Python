ea60b0285b5a83da84f5a518909aae6829a4f7ff
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-24 10:37:34 +0500

    .

---
+++ right-sided-decreasing-triangle-program/while.py
@@ -0,0 +1,19 @@
+i = 1
+while i <= 5:
+    j = 0
+    while j < i:
+        print(" ",end=" ")
+        j += 1
+    p = 5
+    while p >= i:
+        print("$",end=" ")
+        p  -= 1
+    print() 
+    i += 1
+#   $ $ $ $ $ 
+#     $ $ $ $ 
+#       $ $ $
+#         $ $
+#           $
+        
+          
