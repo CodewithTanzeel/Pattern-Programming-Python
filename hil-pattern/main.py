7bb47a6513e121b447dd5c6f78ac6c1f7da579ff
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-25 22:18:28 +0500

    .

--- hil-pattern/main.py
+++ hil-pattern/main.py
@@ -7,9 +7,13 @@ for i in  range(n):
         print(' ',end=' ')
     for j in range(i):
         print('*',end=' ')
-    
     for j in range(i+1):
         print('*',end=' ')
     
+    
     print()
     
+
+
+
+# Reverse hill pattern
