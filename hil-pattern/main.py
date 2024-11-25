2a3fb5ad6572065142597987d4374a4deb7f18c3
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-25 22:27:21 +0500

    .

--- hil-pattern/main.py
+++ hil-pattern/main.py
@@ -1,5 +1,5 @@
 # Hill Pattern Basic
-
+print("This is a  hill pattern")
 n = 5 
 for i in  range(n):
 
@@ -14,6 +14,20 @@ for i in  range(n):
     print()
     
 
-
-
 # Reverse hill pattern
+
+print("This is a reverse hill pattern")
+n = 5 
+for i in  range(n):
+
+    for j in range(i+1):
+        print(' ',end=' ')
+    for j in range(i,n-1):  # -1 is important to make both of the decreasing triangle symmetric
+        print('*',end=' ')
+    for j in range(i,n):
+        print('*',end=' ')
+    
+    
+    print()
+    
+
