64f2800b2ad00c466a43822536057c2295705a19
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-12-06 17:39:03 +0500

    .

--- hollow-pattern-programs/main.py
+++ hollow-pattern-programs/main.py
@@ -84,14 +84,14 @@
 
 # PLUS sign
 
-# n = 5 
-# for i in range(n):
-#    for j in range(n):
-#       if (i == n//2 or j == n // 2):
-#          print('*',end=' ')
-#       else:
-#          print(' ',end=' ')
-#    print()
+n = 5 
+for i in range(n):
+   for j in range(n):
+      if (i == n//2 or j == n // 2):
+         print('*',end=' ')
+      else:
+         print(' ',end=' ')
+   print()
 
 #ouput :
 #     *     
@@ -119,14 +119,14 @@
 
 # Hollow square pattern 
 
-# n = 5 
-# for i in range(n):
-#    for j in range(n):
-#       if (i == 0 or i == n - 1 or j == 0 or j == n -1):
-#          print('*',end=' ')
-#       else:
-#          print(' ',end=' ')
-#    print()
+n = 5 
+for i in range(n):
+   for j in range(n):
+      if (i == 0 or i == n - 1 or j == 0 or j == n -1):
+         print('*',end=' ')
+      else:
+         print(' ',end=' ')
+   print()
 # output 
 # * * * * *
 # *       *
@@ -149,15 +149,15 @@
 # * * * *
 # * * * * *
 
-# n = 5
-# for i in range(n):
-#    for j in range(i+1):
-#     if (i == n-1 or j == i or j == 0):
-#        print('*',end=' ')
-#     else:
-#        print(' ',end=' ')
+n = 5
+for i in range(n):
+   for j in range(i+1):
+    if (i == n-1 or j == i or j == 0):
+       print('*',end=' ')
+    else:
+       print(' ',end=' ')
 
-#    print()
+   print()
 
 # output
 # * 
@@ -166,15 +166,15 @@
 # *     *
 # * * * * *
 
-# n = 5
-# for i in range(n):
-#    for j in range(i,n):
-#     if (i == 0 or j == i or j == n-1):
-#        print('*',end=' ')
-#     else:
-#        print(' ',end=' ')
+n = 5
+for i in range(n):
+   for j in range(i,n):
+    if (i == 0 or j == i or j == n-1):
+       print('*',end=' ')
+    else:
+       print(' ',end=' ')
 
-#    print()
+   print()
 
 #Ouput
 # * * * * *
