872225c485b7da226b16a0258d6d02d5145d32f8
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-12-24 22:58:46 +0500

    .

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-# print a star
-print("*")
-# For instance if you want to write stars in a row
-i = 6
-print("*"*i)#first implementation using print statment
-# Another implementation using for loop
-i = 6
-for j in range(i):
-    print("*")
-# But the output of above program will be printed on next row by each iteration
-i = 6
-for j in range(i):
-    print("*",end="")
-print()
+# # print a star
+# print("*")
+# # For instance if you want to write stars in a row
+# i = 6
+# print("*"*i)#first implementation using print statment
+# # Another implementation using for loop
+# i = 6
+# for j in range(i):
+#     print("*")
+# # But the output of above program will be printed on next row by each iteration
+# i = 6
+# for j in range(i):
+#     print("*",end="")
+# print()
 
 # similarly we can improvise and take input from user 
-i = int(input("Enter number of Stars : "))
-for j in range(i+1):
-    print("*")
+# i = int(input("Enter number of Stars : "))
+# for j in range(i+1):
+#     print("*")
 
 # print Square
-n = 5 
-for i in range(n):
-    for j in range(n):
-        print('$',end='  ')
-    print()
-    
-#similary this program can also have input 
-n = int(input("Area of Square input : "))# THIS WILL TAKE YOUR GIVEN INPUT AS LENGTH AND BREATH OF THE SQUARE AS LxB
-item = input("Item that u want to print in square :")# THIS CAN BE ANYTHING EMOJI ETC
-for i in range(n):
-    for j in range(n):
-        print(item,end='  ')
-    print()# Athough this print statment seems useless but it has its significance as it changes the line after each iteration
-    
-
-# # Output 
-# i = 1
-# while i <= 5:
-#     j = 1 
-#     while j <= i:
-#       print("*",end = " ")
-#       j = j + 1
+# n = 5 
+# for i in range(n):
+#     for j in range(n):
+#         print('$',end=' ')
 #     print()
-#     i=i+1
+    
+# #similary this program can also have input 
+# n = int(input("Area of Square input : "))# THIS WILL TAKE YOUR GIVEN INPUT AS LENGTH AND BREATH OF THE SQUARE AS LxB
+# item = input("Item that u want to print in square :")# THIS CAN BE ANYTHING EMOJI ETC
+# for i in range(n):
+#     for j in range(n):
+#         print(item,end='  ')
+#     print()# Athough this print statment seems useless but it has its significance as it changes the line after each iteration
+    
+
+# # # Output 
+i = 1
+while i <= 5:
+    j = 1 
+    while j <= i:
+      print("*",end = " ")
+      j = j + 1
+    print()
+    i=i+1
 
 
-# *
-# * *
-# * * *
-# * * * *
-# * * * * *
+# # *
+# # * *
+# # * * *
+# # * * * *
+# # * * * * *
 
 
-# in while Loop
+# # in while Loop
 i = 1 
 j = 1
 while(i<=8):
@@ -62,12 +62,12 @@ while(i<=8):
     i+=1
     j=1
     print("")
-# output
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
-# * * * * * * * *
+# # output
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
+# # * * * * * * * *
